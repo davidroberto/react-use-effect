7525d97513a33d0f138341c7c03c2317894b3575
David Robert <contact@david-robert.fr>
2022-11-24 16:54:47 +0100

    random cocktail

--- src/App.js
+++ src/App.js
@@ -1,8 +1,27 @@
+import { useState } from "react";
 import CocktailList from "./CocktailList";
 
 function App() {
+
+  const [randomCocktail, setRandomCocktail] = useState(null);
+
+  const displayRandomMeal = async () => {
+      const cocktailResponse = await fetch('https://www.thecocktaildb.com/api/json/v1/1/random.php');
+      const cocktailRandom = await cocktailResponse.json();  
+
+      setRandomCocktail(cocktailRandom.drinks[0])
+  }
+
   return (
-    <CocktailList />
+    <>
+      <button onClick={displayRandomMeal}>Afficher une recette</button>
+      <div>
+        {randomCocktail &&
+          <h2>{randomCocktail.strDrink}</h2>
+        }
+      </div>
+      <CocktailList />
+    </>
   );
 }
 
